adc1b97b1711229359a33dd3f2bbf6156a1e8b54
Glitch (wolfthezelda-boilerplate-project-urlshortener) <none>
2020-03-22 20:27:04 +0000

    ⛸🗾 Updated with Glitch

--- server.js
+++ server.js
@@ -13,11 +13,14 @@ var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -29,6 +32,36 @@ app.get('/', function(req, res){
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+var dns = require("dns");
+var urlSchema = new mongoose.Schema({
+  url: {type: String, required: true},
+  hash: {type: String, required: true}
+});
+var urlModel = mongoose.model("Url", urlSchema);
+app.post("/api/shorturl/new", function (req, res) {
+  var url = req.body.url.replace(/^https?:\/\//i, "");
+  dns.lookup(url, (error, addresses, family) => {
+    if(error) {
+      res.json({error: "invalid URL"});
+    } else {
+      var hash = Math.random().toString(36).substring(7);
+      var model = new urlModel({url, hash});
+      model.save((error, data) => {
+        if(error) {
+          res.json({error: "mongo ERROR"});
+        } else {          
+          res.json({original_url: url, short_url: hash, data});
+        }
+      });
+    };
+  });
+});
+app.get("/api/shorturl/:hash", (req, res) => {
+  urlModel.findOne({hash: req.params.hash }, "url hash", (error, data) => {
+    res.redirect(data != undefined ? ("http://" + data.url) : "/");
+  });
 });
 
 
